2d751a765e4e469ae5b5d84d7c2eade6d2aa186c
Abrar-Rahi <rahiabrar177@gmail.com>
2023-08-16 12:28:10 +0600

    tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <h1>abrar rahi</h1>
+      <h1 className="bg-red-500">abrar rahi</h1>
     </>
   )
 }
